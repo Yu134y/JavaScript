69de255a73771b00ab9c437f2398a3d2c0a19c1a
Yu134y <y.onosaka134@gmail.com>
2022-01-14 17:59:28 +0900

    add:ユーザーの編集と削除を操作する

--- torahack/basic-rest-api/app/public/js/index.js
+++ torahack/basic-rest-api/app/public/js/index.js
@@ -17,6 +17,21 @@ const indexModule = (() => {
                 return location.href = '/';
             });
             break;
+
+        case '/edit.html':
+            const uid = location.search.split('?uid=')[1];
+
+            document.getElementById('save-btn').addEventListener('click', () => {
+                return usersModule.saveUser(uid);
+            });
+            document.getElementById('cancel-btn').addEventListener('click', () => {
+                return location.href = '/';
+            });
+            document.getElementById('delete-btn').addEventListener('click', () => {
+                return usersModule.deleteUser(uid);
+            });
+
+            return usersModule.setExistingValue(uid);
         default:
             break;
     }
